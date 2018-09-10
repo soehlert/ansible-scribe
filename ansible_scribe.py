68082ea69953ce20a496f80cb17a75ceef34b1b8
Sam Oehlert <sam.oehlert@gmail.com>
2018-09-10 01:09:15 -0500

    set up the write license function

--- ansible_scribe.py
+++ ansible_scribe.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
-import os
 import argparse
+import datetime
+import os
+import sys
 import yaml
 
 from configparser import ConfigParser
+from jinja2 import Environment, FileSystemLoader
+
 
 config_dir = os.path.expanduser("~") + "/.config/ansible-scribe"
 global_config = config_dir + "/global.conf"
@@ -15,6 +19,13 @@ parser.add_argument(
     help="Specify config file location",
     default=global_config,
     dest="conf_file",
+)
+parser.add_argument(
+    "-o",
+    "--overwrite",
+    help="Overwrite the files in place in the role directory",
+    default=False,
+    dest="overwrite",
 )
 parser.add_argument(
     "--version", help="Print version", action="version", version="%(prog)s 0.1.0-alpha"
@@ -56,18 +67,42 @@ def read_config(role):
 
 
 def read_tasks(role):
-    """ Reads in task files to build variables list """
+    """ Reads in task files to build variables list and task list """
     pass
 
 
 def read_defaults(role):
     """ Read the defaults file for the role to check if it's filled out """
-    pass
+    roles = settings["roles_path"]
+    defaults_dir = os.path.join(roles, role, "defaults")
+    defaults = os.path.join(defaults_dir, "defaults.yml")
+    with open(defaults, "r+") as d:
+        data = yaml.safe_load(d)
+        print(data)
 
 
-def write_template(template_file, path):
+def get_templates_path():
+    """ Help us figure out where the script is running from so we can find
+    files we need """
+    script_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+    templates = os.path.join(script_path, "templates")
+    return templates
+
+
+def write_license(license, author, path):
     """ Writes out a template file for a role """
-    pass
+    now = datetime.datetime.now()
+    year = now.year
+    licenses = os.path.join(get_templates_path(), "licenses")
+    licensej2 = "{}.j2".format(license)
+    template_loader = FileSystemLoader(searchpath=licenses)
+    template_env = Environment(loader=template_loader)
+    template = template_env.get_template(licensej2)
+    data = template.render(copyright_holder=settings["author"], year=year)
+
+    role_license = os.path.join(settings["roles_path"], licensej2)
+    with open(role_license, "wb") as rl:
+        rl.write(data)
 
 
 if __name__ == "__main__":
